6ae8667a413657686c35f24c12c32848ca0053d6
Alice Benedetti <54101528+adeadelaide@users.noreply.github.com>
2024-06-26 19:25:37 +0300

    1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,61 @@
+const Header = ({title}) => {
+  return (<h1>{title}</h1>)
+}
+
+const Part = ({part}) => {
+  const { name, excercises } = part;
+  
+  return (<li>{name} {excercises}</li>)
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />      <br />
+    </div>
+  )
+}
+
+const Total = ({total}) => {
+  return(
+    <>
+     Total number of exercises: {total}
+    </>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const totExercises = part1.exercises + part2.exercises + part3.exercises
+  const course =  'Half Stack application development'
+  const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  
+  
+  const totExercises = parts.reduce((total, part) => total + part.exercises, 0);
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1.name}: {part1.exercises}</p>
-      <p>{part2.name}: {part2.exercises}</p>
-      <p>{part3.name}: {part3.exercises}</p>
-      <p>Total number of exercises: {totExercises}</p>
-
+      <Header title={course} />
+      <p><b>Table of contents:</b></p>
+      <Content parts={parts} />
+      <Total total={totExercises} />
     </div>
   )
 }
 
 export default App
+
+
